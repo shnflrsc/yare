e937781d37d3c83dd24dc48a56f6a9a5314a3dfe
Darl Shanen Floresca <floresca.shanegarcia@gmail.com>
2026-09-11 03:04:11 +0800

    Initial Commit

---
+++ src/main/java/io/shnflrsc/yare/YareApplication.java
@@ -0,0 +1,13 @@
+package io.shnflrsc.yare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/shnflrsc/yare/YareApplicationTests.java
@@ -0,0 +1,13 @@
+package io.shnflrsc.yare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
