dc550eb3821001c69b384c5a26d68d90c519cc3c
Darl Shanen Floresca <floresca.shanegarcia@gmail.com>
2026-09-11 03:28:30 +0800

    feat: add aws sdk and flyway dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,6 +18,10 @@ repositories {
 }
 
 dependencies {
+	implementation(platform("software.amazon.awssdk:bom:2.48.0"))
+	implementation("software.amazon.awssdk:s3")
+	implementation("org.springframework.boot:spring-boot-starter-flyway")
+	implementation("org.flywaydb:flyway-database-postgresql")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-webmvc")
 	compileOnly("org.projectlombok:lombok")
